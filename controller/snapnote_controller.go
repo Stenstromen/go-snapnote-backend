daf7c48578bed6d0f994614fe6009d3f235e41b4
Stenstromen <104073740+Stenstromen@users.noreply.github.com>
2023-05-25 00:01:48 +0200

    Used MVC model

---
+++ controller/snapnote_controller.go
@@ -0,0 +1,57 @@
+package controller
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/stenstromen/go-snapnote-backend/model"
+	"github.com/stenstromen/go-snapnote-backend/service"
+)
+
+func CreateFormData(w http.ResponseWriter, r *http.Request) {
+	var formData model.FormData
+	err := json.NewDecoder(r.Body).Decode(&formData)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	err = service.InsertFormData(formData)
+	if err != nil {
+		http.Error(w, "Failed to insert data", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	fmt.Fprintln(w, "Data inserted successfully")
+}
+
+func GetFormData(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	noteID := vars["noteid"]
+
+	fmt.Println("noteid:", noteID)
+
+	formData, err := service.GetFormDataByNoteID(noteID)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			http.Error(w, "Note not found", http.StatusNotFound)
+		} else {
+			http.Error(w, "Failed to retrieve data", http.StatusInternalServerError)
+		}
+		return
+	}
+
+	jsonData, err := json.Marshal(formData)
+	if err != nil {
+		http.Error(w, "Failed to marshal JSON", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(jsonData)
+}
